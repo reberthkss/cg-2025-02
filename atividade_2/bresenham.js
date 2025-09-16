839f8d118f0f3fce561bcdde762f103969364a40
Reberth <reberthkss@outlook.com>
2025-09-16 20:48:33 -0300

    feat: bresenham improvement

--- atividade_2/bresenham.js
+++ atividade_2/bresenham.js
@@ -1,33 +1,34 @@
 function bresenham(p0, p1) {
-    const x0 = p0[0];
-    const y0 = p0[1];
-    const x1 = p1[0];
-    const y1 = p1[1];
+    let x0 = p0[0], y0 = p0[1];
+    let x1 = p1[0], y1 = p1[1];
 
     const line = [];
+    
+    let dx = Math.abs(x1 - x0);
+    let dy = Math.abs(y1 - y0);
+    
+    let sx = x0 < x1 ? 1 : -1;
+    let sy = y0 < y1 ? 1 : -1;
+    
+    let err = dx - dy;
+    let x = x0, y = y0;
 
-    const dx = Math.abs(x1 - x0);
-    const dy = Math.abs(y1 - y0);
-
-    let decisionFactor = 2*dy-dx;
-
-    let incInf = 2*dy;
-    let incSup = 2*(dy-dx);
-
-    let x = x0;
-    let y = y0;
-
-    line.push([x, y]);
-
-    while (x < x1) {
-        if (decisionFactor < 0) {
-            decisionFactor = decisionFactor + incInf;
-        } else {
-            decisionFactor = decisionFactor + incSup;
-            y++;
-        }
-        x++;
+    while (true) {
         line.push([x, y]);
+        
+        if (x === x1 && y === y1) break;
+        
+        let e2 = 2 * err;
+        
+        if (e2 > -dy) {
+            err -= dy;
+            x += sx;
+        }
+        
+        if (e2 < dx) {
+            err += dx;
+            y += sy;
+        }
     }
 
     return line;
