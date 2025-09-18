e16cf9f84e6c99bf7cf57b4adc404619410dce46
Reberth <reberthkss@outlook.com>
2025-09-17 23:26:16 -0300

    code review

--- atividade_3_bresenham_circunferencia/bresenham_circunference.js
+++ atividade_3_bresenham_circunferencia/bresenham_circunference.js
@@ -3,9 +3,8 @@ function bresenhamCircle(centerX, centerY, radius) {
     
     let x = 0;
     let y = radius;
-    let d = 3 - 2 * radius; // Termo de decisão inicial
+    let d = 3 - 2 * radius;
     
-    // Função auxiliar para adicionar os 8 pontos simétricos
     function addCirclePoints(cx, cy, x, y) {
         const points = [];
         points.push([cx + x, cy + y]);  // Octante 1
@@ -25,7 +24,6 @@ function bresenhamCircle(centerX, centerY, radius) {
     while (y >= x) {
         x++;
         
-        // Atualizar termo de decisão
         if (d > 0) {
             y--;
             d = d + 4 * (x - y) + 10;
@@ -33,17 +31,8 @@ function bresenhamCircle(centerX, centerY, radius) {
             d = d + 4 * x + 6;
         }
         
-        // Adicionar os 8 pontos simétricos
         circlePoints.push(...addCirclePoints(centerX, centerY, x, y));
     }
     
     return circlePoints;
-}
-
-function bresenhamCircleFromCenterAndPoint(center, radius) {
-    // Calcular raio a partir do centro e um ponto na circunferência
-    const dx = point[0] - center[0];
-    const dy = point[1] - center[1];
-    
-    return bresenhamCircle(center[0], center[1], radius);
 }
