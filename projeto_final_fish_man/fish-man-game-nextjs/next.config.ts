40ca7fbad3a638ed4d3cca8db1c55a1eae90d478
Reberth <reberthkss@outlook.com>
2025-12-13 13:24:35 -0300

    fix

--- projeto_final_fish_man/fish-man-game-nextjs/next.config.ts
+++ projeto_final_fish_man/fish-man-game-nextjs/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export', // Enables static exports
+  basePath: '/cg-2025-02', // Replace with your repository name (e.g., '/my-portfolio')
+  assetPrefix: '/cg-2025-02/', // Required for correct asset loading
+  images: {
+    unoptimized: true, // GitHub Pages does not support Next.js image optimization
+  },
 };
 
 export default nextConfig;
